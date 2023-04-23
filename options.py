ff8580eefdd318f1275e669d4c44e3fe6405a419
Vitaly Ruda <rudavitalij@yandex.ru>
2023-04-23 13:34:22 +0300

    Update options for prev commit

--- options.py
+++ options.py
@@ -1,5 +1,11 @@
-#Name of image sender. Will be shown in discord. May be any. By default is username in Windows
-username = '%USERNAME%'
+author = {
+    #Auhor name. Will be shown in discord. May be any. By default is username in Windows
+    'name': '%username%',
+    #author avatar url
+    'icon_url': '',
+    #post color
+    'color': 0
+}
 #directory to search screenshots
 searchdir = '%appdata%/.minecraft/screenshots'
 #max age of screenshot file
@@ -7,6 +13,7 @@ max_file_age = 30 #seconds
 #max age of screenshot file for first check after run
 max_file_age_first = 90 #seconds
 #discord webhook url
-chanel_url = ""
+chanel_url = ''
 #remove file after send to discord
 remove_screenshot_file = False
+use_logging = False
